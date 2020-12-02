b1d97aa7c57b4482038c94943992522292c28c49
Michal Tuček <michaltk1@gmail.com>
2020-12-02 22:18:19 +0100

    Fixed getBody method in Request for POST

--- core/Request.php
+++ core/Request.php
@@ -50,7 +50,7 @@ class Request
             }
         }
         if ($this->method() === 'post') {
-            foreach ($_GET as $key => $value) {
+            foreach ($_POST as $key => $value) {
                 $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
             }
         }
