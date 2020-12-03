4506ef0310c95cfd460ba7a4bead866c768add2b
Michal Tuček <michaltk1@gmail.com>
2020-12-03 19:34:58 +0100

    Added field types for Field

--- core/Form/Field.php
+++ core/Form/Field.php
@@ -12,8 +12,13 @@ use Okami\Core\Model;
  */
 class Field
 {
-    public Model $model;
+    public const TYPE_TEXT = 'text';
+    public const TYPE_EMAIL = 'email';
+    public const TYPE_PASSWORD = 'password';
+    public const TYPE_NUMBER = 'number';
 
+    public string $type;
+    public Model $model;
     public string $attribute;
 
     /**
@@ -24,6 +29,7 @@ class Field
      */
     public function __construct(Model $model, string $attribute)
     {
+        $this->type = self::TYPE_TEXT;
         $this->model = $model;
         $this->attribute = $attribute;
     }
@@ -33,17 +39,30 @@ class Field
         return sprintf('
             <div class="form-group">
                 <label>%s</label>
-                <input type="text" name="%s" value="%s" class="form-control%s">
+                <input type="%s" name="%s" value="%s" class="form-control%s">
                 <div class="invalid-feedback">
                     %s
                 </div>
             </div>
         ',
             $this->attribute,
+            $this->type,
             $this->attribute,
             $this->model->{$this->attribute},
             $this->model->hasError($this->attribute) ? ' is-invalid' : '',
             $this->model->getFirstError($this->attribute)
         );
+    }
+
+    public function emailField()
+    {
+        $this->type = self::TYPE_EMAIL;
+        return $this;
+    }
+
+    public function passwordField()
+    {
+        $this->type = self::TYPE_PASSWORD;
+        return $this;
     }
 }
