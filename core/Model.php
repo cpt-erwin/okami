dd779fcee531116c925458c15637987e2b6521b0
Michal Tuček <michaltk1@gmail.com>
2020-12-05 21:59:10 +0100

    Added new validation rule UNIQUE

--- core/Model.php
+++ core/Model.php
@@ -15,6 +15,7 @@ abstract class Model
     public const RULE_MIN = 'min';
     public const RULE_MAX = 'max';
     public const RULE_MATCH = 'match';
+    public const RULE_UNIQUE = 'unique';
 
     public array $errors = [];
 
@@ -62,6 +63,18 @@ abstract class Model
                 if ($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
                     $this->addError($attribute, self::RULE_MAX, $rule);
                 }
+                if ($ruleName === self::RULE_UNIQUE) {
+                    $classMame = $rule['class'];
+                    $uniqueAttribute = $rule['attribute'] ?? $attribute;
+                    $tableName = $classMame::tableName();
+                    $statement = App::$app->db->pdo->prepare("SELECT * FROM $tableName WHERE $uniqueAttribute = :attribute;");
+                    $statement->bindValue(":attribute", $value);
+                    $statement->execute();
+                    $record = $statement->fetch();
+                    if ($record) {
+                        $this->addError($attribute, self::RULE_UNIQUE, ['field' => $attribute]);
+                    }
+                }
             }
         }
 
@@ -92,6 +105,7 @@ abstract class Model
             self::RULE_MIN => 'Min length of this field must be {min}',
             self::RULE_MAX => 'Max length of this field must be {max}',
             self::RULE_MATCH => 'This field must be same as {match}',
+            self::RULE_UNIQUE => 'Record with this {field} already exists',
         ];
     }
 
