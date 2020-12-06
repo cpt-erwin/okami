854540f0f974d6c0816ab8718ad6e15fe027ef3d
Michal Tuček <michaltk1@gmail.com>
2020-12-06 22:21:14 +0100

    Added Session class to handle sessions

--- core/App.php
+++ core/App.php
@@ -14,6 +14,7 @@ class App
     public Router $router;
     public Request $request;
     public Response $response;
+    public Session $session;
     public Database $db;
     public static App $app;
     public Controller $controller;
@@ -24,6 +25,7 @@ class App
         self::$app = $this;
         $this->request = new Request();
         $this->response = new Response();
+        $this->session = new Session();
         $this->router = new Router($this->request, $this->response);
 
         $this->db = new Database($config['db']);
