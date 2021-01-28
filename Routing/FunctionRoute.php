a9854821a8c6154ec9301f94802e3a45680c879f
Michal Tuček <michaltk1@gmail.com>
2021-01-28 13:29:25 +0100

    Implemented logic for specific Route types

--- Routing/FunctionRoute.php
+++ Routing/FunctionRoute.php
@@ -2,12 +2,21 @@
 
 namespace Okami\Core\Routing;
 
+use Okami\Core\App;
+use Okami\Core\Response;
+
 /**
  * Class FunctionRoute
  *
  * @author Michal Tuček <michaltk1@gmail.com>
  * @package Okami\Core\Routing
  */
-abstract class FunctionRoute extends Route
+class FunctionRoute extends Route
 {
+    public function handleCallback(): Response
+    {
+        $response = new Response();
+        $response->body = call_user_func($this->getCallback(), App::$app->request, App::$app->response, $this->getParams());
+        return $response;
+    }
 }
